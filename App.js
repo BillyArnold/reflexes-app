4d30bdd09228641bcf5af1c6838c441cfc3a57ff
Billy Arnold <billyarnoldd@gmail.com>
2024-01-26 20:27:04 +0000

    Adding a ball and moving it around the screen on press animated

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from "react-native-reanimated";
 
 export default function App() {
+  const valueX = useSharedValue(Math.floor(Math.random() * 99));
+  const valueY = useSharedValue(Math.floor(Math.random() * 99));
+
+  const style = useAnimatedStyle(() => {
+    return {
+      left: withTiming(valueX.value, {
+        duration: 500,
+        easing: Easing.bezier(0.5, 0.01, 0, 1),
+      }),
+      top: withTiming(valueY.value, {
+        duration: 500,
+        easing: Easing.bezier(0.5, 0.01, 0, 1),
+      }),
+    };
+  });
+
+  const handleTap = () => {
+    valueX.value = Math.floor(Math.random() * 99);
+    valueY.value = Math.floor(Math.random() * 99);
+  };
   return (
     <View className="bg-black flex justify-center items-center h-full">
-      <Text className="text-white">
-        Open up App.js to start working on your app!
-      </Text>
+      <Pressable onPress={() => handleTap()}>
+        <Animated.View
+          style={[style]}
+          className={`absolute flex w-5 h-5 bg-white rounded-full`}
+        ></Animated.View>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
