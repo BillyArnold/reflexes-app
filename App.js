fc1bdf6eea6e54e813c99598da294b5d91fe7623
Billy Arnold <billyarnoldd@gmail.com>
2024-01-27 13:24:33 +0000

    Updating some styling and animation organisation

--- App.js
+++ App.js
@@ -9,35 +9,51 @@ import Animated, {
 } from "react-native-reanimated";
 
 export default function App() {
-  const valueX = useSharedValue(Math.floor(Math.random() * 99));
-  const valueY = useSharedValue(Math.floor(Math.random() * 99));
+  const valueX = useSharedValue(Math.floor(Math.random() * 90));
+  const valueY = useSharedValue(Math.floor(Math.random() * 90));
+  const [count, setCount] = useState(0);
 
   const style = useAnimatedStyle(() => {
     return {
-      left: withTiming(valueX.value, {
-        duration: 500,
+      left: withTiming(valueX.value + "%", {
+        duration: 400,
         easing: Easing.bezier(0.5, 0.01, 0, 1),
       }),
-      top: withTiming(valueY.value, {
-        duration: 500,
-        easing: Easing.bezier(0.5, 0.01, 0, 1),
+      top: withTiming(valueY.value + "%", {
+        duration: 400,
+        easing: Easing.bezier(0.5, 0.08, 0, 1),
       }),
     };
   });
 
+  const calculateNewPosition = (prev) => {
+    if (prev > 50) {
+      return Math.floor(Math.random() * 50);
+    } else {
+      return Math.floor(Math.random() * 50) + 40;
+    }
+  };
+
   const handleTap = () => {
-    valueX.value = Math.floor(Math.random() * 99);
-    valueY.value = Math.floor(Math.random() * 99);
+    valueX.value = calculateNewPosition(valueX.value);
+    valueY.value = calculateNewPosition(valueY.value);
+    setCount((prev) => (prev += 1));
   };
   return (
-    <View className="bg-black flex justify-center items-center h-full">
-      <Pressable onPress={() => handleTap()}>
-        <Animated.View
-          style={[style]}
-          className={`absolute flex w-5 h-5 bg-white rounded-full`}
-        ></Animated.View>
-      </Pressable>
-      <StatusBar style="auto" />
+    <View className="bg-gray-800 flex justify-center items-center h-full z-50">
+      <View className="absolute right-4 top-10">
+        <Text className="text-white">Tap Count: {count}</Text>
+      </View>
+      <Animated.View
+        style={[style]}
+        className="absolute left-0 top-0 flex w-10 h-10 bg-white shadow-2xl shadow-blue-50/80 rounded-full"
+      >
+        <Pressable
+          className="absolute w-full h-full top-0 left-0"
+          onPress={() => handleTap()}
+        ></Pressable>
+      </Animated.View>
+      <StatusBar style="light" />
     </View>
   );
 }
